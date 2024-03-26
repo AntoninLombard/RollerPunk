8fe67c911158e243e0fedf49d6b7330a2ef7585b
LOMBARD Antonin <antonin.lombard.auditeur@lecnam.net>
2024-03-26 17:27:49 +0100

    Player controller V0 fix

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,16 +10,21 @@ public class PlayerController : MonoBehaviour
     
     [Header("DRIVING VALUES")]
     [SerializeField] private float forwardAccel = 0;
-    [SerializeField] private float brakingAccel = 0;
+    [SerializeField] private float brakingRatio = 0;
     [SerializeField] private float turningRate = 0;
     [SerializeField] private float gravityStrength = 0.0f;
     
+    
+    [Header("DRIVING STATE (DO NOT MODIFY)")]
     [SerializeField] private bool isAccelerating = false;
     [SerializeField] private bool isBraking = false;
     [SerializeField] private bool isTurning = false;
+    [SerializeField] private bool isGrounded = false;
 
-    [SerializeField] private float acceleration = 0;
-    [SerializeField] private float turningValue = 0;
+    [SerializeField] private float speed = 0;
+    [SerializeField] private float currentSpeed = 0;
+    [SerializeField] private float turningInput = 0;
+    [SerializeField] private float accelInput = 0;
 
 
     [SerializeField] private Transform character;
@@ -45,25 +50,30 @@ public class PlayerController : MonoBehaviour
     {
         transform.position = rb.position;
 
-        if (Input.GetAxis("Vertical") > 0f)
+        turningInput = Input.GetAxis("Horizontal");
+        accelInput = Input.GetAxis("Vertical");
+        
+        
+        if (accelInput > 0f)
         {
             isBraking = false;
             isAccelerating = true;
+            currentSpeed = currentSpeed + forwardAccel * Time.deltaTime;
         }
-        else if(Input.GetAxis("Vertical") < 0f)
+        else if(accelInput < 0f)
         {
             isBraking = true;
             isAccelerating = false;
+            currentSpeed = currentSpeed * brakingRatio * Time.deltaTime;
         } else
         {
             isBraking = false;
             isAccelerating = false;
         }
 
-        if (Input.GetAxis("Horizontal") != 0)
+        if (turningInput != 0)
         {
             isTurning = true;
-            turningValue = Input.GetAxis("Horizontal");
         }
         else
         {
@@ -73,26 +83,49 @@ public class PlayerController : MonoBehaviour
     
     void FixedUpdate()
     {
+        
+        rb.AddForce(transform.forward * currentSpeed, ForceMode.Acceleration);
+        
+        
+        /*//Speeding up the player
         if (isAccelerating & !isBraking)
         {
-            rb.AddForce(transform.forward * forwardAccel,ForceMode.Acceleration);
+            rb.AddForce(transform.forward * (forwardAccel * accelInput), ForceMode.Acceleration);
         }
 
+        
+        //Slowing down the player when braking
         if (isBraking && rb.velocity.magnitude > 0)
         {
-            rb.AddForce(-transform.forward * (brakingAccel * rb.velocity.magnitude),ForceMode.Acceleration);
-        }
+            rb.AddForce(transform.forward * (brakingAccel * rb.velocity.magnitude * accelInput));
+        }*/
 
+        
+        
+        //Turning the player toward the new direction
         if (isTurning)
         {
-            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0,transform.eulerAngles.y + turningValue * turningRate, 0), Time.deltaTime * 5f);
+            isGrounded = true;
+            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0,transform.eulerAngles.y + turningInput * turningRate, 0), Time.deltaTime * 5f);
         }
         
-        
-        RaycastHit hitOn;
-        Physics.Raycast(character.position + (character.up * .1f), Vector3.down, out hitOn);
-        character.up = Vector3.Lerp(character.up, hitOn.normal, Time.deltaTime * 8.0f);
+        //Applying gravity force down ward
         //rb.AddForce(Vector3.down * gravityStrength, ForceMode.Acceleration);
+        
+        //Adjust character model to the surface normal it's on
+        RaycastHit hitOn;
+        if (Physics.Raycast(character.position, Vector3.down * 0.5f, out hitOn))
+        {
+            isGrounded = true;
+            character.up = Vector3.Lerp(character.up, hitOn.normal, Time.deltaTime * 8.0f);
+            character.Rotate(0, transform.eulerAngles.y, 0);
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        speed = rb.velocity.magnitude;
     }
     
     #endregion
