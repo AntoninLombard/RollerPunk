15631a82ef5b0c46ad700c9a9a163dcec37a07e8
Flo <spartiate903@gmail.com>
2024-06-13 19:05:49 +0200

    slash vfx, drift vfx, scaled down colliders

--- Assets/Graphic assets/VFX/VfxManager.cs
+++ Assets/Graphic assets/VFX/VfxManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.VFX;
 
 public class VfxManager : MonoBehaviour
 {
-    [SerializeField] VisualEffect punchL, punchR, slashL, slashR, shield ;
+    [SerializeField] VisualEffect punchL, punchR, slashL, slashR, slashTornado01, slashTornado02, shield, DSparksHandL, DSparksHandR, DSparksWheelL, DSparksWheelR ;
 
     public void playShield() {
         shield.Play() ;
@@ -25,5 +25,40 @@ public class VfxManager : MonoBehaviour
     
     public void playSlashR() {
         slashR.Play() ;
+    }
+
+    public void ParrySuccess() {
+        slashTornado01.Play() ;
+        slashTornado02.Play() ;
+    }
+
+    public void driftStart(int direction) {
+        if (direction == -1) {
+            DSparksHandL.Play() ;
+            DSparksWheelL.Play() ;
+        } else {
+            DSparksHandR.Play() ;
+            DSparksWheelR.Play() ;
+        }
+    }
+
+    public void onBoostReady(int direction) {
+        if (direction == -1) {
+            DSparksHandL.SendEvent("OnBoostReady") ;
+            DSparksWheelL.SendEvent("OnBoostReady") ;
+        } else {
+            DSparksHandR.SendEvent("OnBoostReady") ;
+            DSparksWheelR.SendEvent("OnBoostReady") ;
+        }
+    }
+    public void driftStop(int direction) {
+        if (direction == -1) {
+            DSparksHandL.Stop() ;
+            DSparksWheelL.Stop() ;
+        } else {
+            DSparksHandR.Stop() ;
+            DSparksWheelR.Stop() ;
+        }
+        
     }
 }
